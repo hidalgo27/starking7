55b124a08006ac77da99a3bbe2b81a2e0e736dab
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-07-07 08:31:57 -0500

    update

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -61,42 +61,42 @@
             {{--        </div>--}}
             <div class="slider-area slider-bg-5 relative-position">
                 <div class="slider-text">
-                    <div class="layer-1-2">
-                        <div class="coming-countdown ul-li">
-                            <h4 class="text-warning pb-4 d-block font-weight-bold">INICO DE CLASES 15 DE MAYO <br> SEPARA TU VACANTE</h4>
-                            <ul>
-                                <li class="days">
-                                    <span class="number"></span>
-                                    <span class>Días</span>
-                                </li>
+{{--                    <div class="layer-1-2">--}}
+{{--                        <div class="coming-countdown ul-li">--}}
+{{--                            <h4 class="text-warning pb-4 d-block font-weight-bold">INICO DE CLASES 15 DE MAYO <br> SEPARA TU VACANTE</h4>--}}
+{{--                            <ul>--}}
+{{--                                <li class="days">--}}
+{{--                                    <span class="number"></span>--}}
+{{--                                    <span class>Días</span>--}}
+{{--                                </li>--}}
 
-                                <li class="hours">
-                                    <span class="number"></span>
-                                    <span class>Horas</span>
-                                </li>
+{{--                                <li class="hours">--}}
+{{--                                    <span class="number"></span>--}}
+{{--                                    <span class>Horas</span>--}}
+{{--                                </li>--}}
 
-                                <li class="minutes">
-                                    <span class="number"></span>
-                                    <span class>Minutos</span>
-                                </li>
+{{--                                <li class="minutes">--}}
+{{--                                    <span class="number"></span>--}}
+{{--                                    <span class>Minutos</span>--}}
+{{--                                </li>--}}
 
-                                <li class="seconds">
-                                    <span class="number"></span>
-                                    <span class>Segundos</span>
-                                </li>
-                            </ul>
+{{--                                <li class="seconds">--}}
+{{--                                    <span class="number"></span>--}}
+{{--                                    <span class>Segundos</span>--}}
+{{--                                </li>--}}
+{{--                            </ul>--}}
 
-                        </div>
-                    </div>
+{{--                        </div>--}}
+{{--                    </div>--}}
                     <div class="section-title mb20 headline text-center ">
                         <div class="layer-1-3">
-                            <h1 class="d-none d-md-block third-slide display-3 font-weight-bold text-purple mb-sm-3 rgba-white-6 p-3 rounded shadow"><span>LEAN AGILE PROJECT MANAGEMENT</span></h1>
+                            <h1 class="d-none d-md-block third-slide display-3 font-weight-bold text-purple mb-sm-3 rgba-white-6 p-3 rounded shadow"><span>LEAN AGILE PROJECT <br> MANAGEMENT</span></h1>
                             <h1 class="d-md-none third-slide font-weight-bold text-purple mb-sm-3 rgba-white-6 p-3 rounded shadow"><span>LEAN AGILE PROJECT MANAGEMENT</span></h1>
 {{--                            <h4 class="mb-3 font-weight-bold text-white">EN EDIFICACIONES</h4>--}}
                         </div>
                     </div>
-                    <div class="layer-1-4">
-                        <div class="about-btn text-center">
+                    <div class="layer-1-4 mb-6">
+                        <div class="about-btn text-center relative z-50">
                             <div class="genius-btn text-center text-uppercase ul-li-block bold-font">
                                 <a href="{{route('landingpmi')}}">Ver más <i class="fas fa-caret-right"></i></a>
                             </div>
