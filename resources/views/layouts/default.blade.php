29c651a571a77f8f0927712ee47b56a93b332562
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-08-20 09:05:58 -0500

    Update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -82,6 +82,14 @@
     {{--    <link href="{{asset('css/colors/color-8.css')}}" rel="alternate stylesheet" type="text/css" title="color-8">--}}
     {{--    <link href="{{asset('css/colors/color-9.css')}}" rel="alternate stylesheet" type="text/css" title="color-9">--}}
 
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Facebook Pixel Code -->
     <script>
         !function(f,b,e,v,n,t,s)
